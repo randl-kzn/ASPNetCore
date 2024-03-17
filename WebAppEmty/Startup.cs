37e4023ce6221e8fd38e2fa590bcb876f8ce6418
Rinat Khabibullin <khrinat11@gmail.com>
2024-03-17 16:05:40 +0300

    Замените на метод расширения MapRazorPages() . Для простоты также пока удалите существующий компонент обработ- чика ошибок.

--- WebAppEmty/Startup.cs
+++ WebAppEmty/Startup.cs
@@ -22,19 +22,10 @@ namespace WebAppEmty
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapRazorPages();
             });
         }
     }
